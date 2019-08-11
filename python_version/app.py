25d45b5cb7d852199205b8ff10d98728b860efe9
Angus <angus@AngusL-MacBook-Pro.local>
2019-08-11 10:49:00 +1000

    doneso

--- python_version/app.py
+++ python_version/app.py
@@ -9,7 +9,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 
-cred = credentials.Certificate('../../../desktop/firebase.json')
+cred = credentials.Certificate('./serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
